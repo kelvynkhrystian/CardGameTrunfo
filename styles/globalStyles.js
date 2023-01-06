7edcd44f0ab31ecf5da0c8147b211f971e709c60
Kelvyn Khrystian <khrystian.dev@hotmail.com>
2023-01-06 00:25:20 -0300

    generate-global-styles

---
+++ styles/globalStyles.js
@@ -0,0 +1,25 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body {
+
+  }
+
+  #root {
+
+  }
+
+  @media (min-width:600px) {
+    body {
+      
+    }
+  }
+`;
+
+export default GlobalStyle;
